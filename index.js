bdc17f10a2661f19b4ba46336693baf3b5a9bb30
Nawaf Almutairi <almutain1@roehampton.ac.uk>
2025-03-12 13:04:42 +0000

    Docker setup with pug template

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-"use strict";
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
 
-// Include the app.js file.
-// This will run the code.
-console.log("entrypoint");
-const app = require("./app/app.js");
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static('public'));
+
+// Home route
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Lingolizer Home', message: 'Welcome to Lingolizer!' });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
